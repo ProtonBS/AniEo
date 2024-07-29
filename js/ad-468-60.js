125c23cde24bbb848149064fd8f098e9e22d51ee
ProtonBS <95560567+ProtonBS@users.noreply.github.com>
2024-07-29 09:50:03 +0530

    Create ad-468-60.js

---
+++ js/ad-468-60.js
@@ -0,0 +1,10 @@
+var atOptions = {
+  'key': '6523d843931e77ac152bbee5d25c57aa',
+  'format': 'iframe',
+  'height': 60,
+  'width': 468,
+  'params': {}
+};
+
+// Make sure this script is executed when the DOM is ready or
+// when it is included as part of a page where `initializeAd` is called.
