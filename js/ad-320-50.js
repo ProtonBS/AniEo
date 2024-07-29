6f8da9ce6aec18165ef23a5dc2d29f881e94f407
ProtonBS <95560567+ProtonBS@users.noreply.github.com>
2024-07-29 09:41:28 +0530

    Update ad-320-50.js

--- js/ad-320-50.js
+++ js/ad-320-50.js
@@ -1,26 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Create and append the advertisement note
-  const adNote = document.createElement("p");
-  adNote.textContent = "Advertisement";
+var atOptions = {
+  'key': 'f5944715842e6425397e93e3277a8eab',
+  'format': 'iframe',
+  'height': 50,
+  'width': 320,
+  'params': {}
+};
 
-  // Find the ad container div and append the ad note
-  const adContainer = document.getElementById("ad-container-320-50");
-  adContainer.appendChild(adNote);
-
-  // Ad script options
-  const atOptions = {
-    'key': 'f5944715842e6425397e93e3277a8eab',
-    'format': 'iframe',
-    'height': 50,
-    'width': 320,
-    'params': {}
-  };
-
-  // Create and append the ad script
-  const adScript = document.createElement("script");
-  adScript.type = "text/javascript";
-  adScript.src = "//www.topcreativeformat.com/f5944715842e6425397e93e3277a8eab/invoke.js";
-  
-  // Append the ad script to the ad container
-  adContainer.appendChild(adScript);
-});
+// Make sure this script is executed when the DOM is ready or
+// when it is included as part of a page where `initializeAd` is called.
