0981532cfd9b07d7f1522d2337ef3f00ab0e68c9
ProtonBS <95560567+ProtonBS@users.noreply.github.com>
2024-09-29 16:36:02 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const recentapi = "/recent/";
 
 // Api Server Manager
 
-const AvailableServers = ["https://anidexapi.semant.workers.dev"];
+const AvailableServers = ["https://api.bhoothihu.workers.dev"];
 
 function getApiServer() {
   return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
