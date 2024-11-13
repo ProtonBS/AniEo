7f03b152a5b33ef5f12d44b4c4deae2f8fd5e6ad
ProtonBS <95560567+ProtonBS@users.noreply.github.com>
2024-11-13 07:23:34 +0530

    Update anime.js

--- js/anime.js
+++ js/anime.js
@@ -6,7 +6,7 @@ const recommendationsapi = "/recommendations/";
 
 // Api Server Manager
 
-const AvailableServers = ["https://anidexapi.semant.workers.dev"];
+const AvailableServers = ["https://api-v2.anispace.workers.dev"];
 
 function getApiServer() {
   return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
